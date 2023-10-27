4bd9ad74805fd4c96eccaecccd6fb9fe87660e72
Abhinav Pandey <75622304+AbhinavPandey1911@users.noreply.github.com>
2023-10-27 22:01:53 +0530

    Create Self_Referential_Structures.cpp
    Self-referential classes, also known as recursive data structures

---
+++ C++/Self_Referential_Structures.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() : head(nullptr) {}
+
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << " -> ";
+            current = current->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    std::cout << "Linked List: ";
+    list.display();
+
+    return 0;
+}
